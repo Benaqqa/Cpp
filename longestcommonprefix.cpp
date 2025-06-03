2f94431cfd6265d31683e767c0e81db278c3c9df
unknown <benaqqamoubarak2004@gmail.com>
2025-06-03 22:06:26 +0100

    first commit

---
+++ longestcommonprefix.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int stringcomp(int a,int b){
+    int re = 0;
+    string stra = to_string(a);
+    b = (string)b;
+
+}
+
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        for (int i =0;i<arr1.size();i++){
+            for (int j =0;j<arr2.size();j++){
+
+            }
+        }
+    }
+};
